d70c80c27ac6a06aac9a493a81c26bab4373b8c5
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2023-07-26 13:06:01 +0200

    Changed stuff for PoryMap compatibility

--- include/constants/maps.h
+++ include/constants/maps.h
@@ -4,7 +4,7 @@
 #include "constants/map_groups.h"
 #include "constants/map_types.h"
 
-#define MAP_NONE (0x7F | (0x7F << 8))
+#define MAP_DYNAMIC (0x7F | (0x7F << 8))
 #define MAP_UNDEFINED (0xFF | (0xFF << 8))
 
 #define MAP_GROUP(map) (MAP_##map >> 8)
