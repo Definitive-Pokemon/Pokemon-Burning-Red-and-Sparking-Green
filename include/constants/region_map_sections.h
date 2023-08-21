cedd31f2fef6112e08cd63fd231d37e52db83684
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2023-08-21 15:46:17 +0200

    Update region_map_sections.h

--- include/constants/region_map_sections.h
+++ include/constants/region_map_sections.h
@@ -222,6 +222,7 @@
 #define MAPSEC_TRAINER_HILL                 0xD4
 #define MAPSEC_NONE                         0xD5
 #define MAPSEC_COUNT                        0xD6
+#define MAPSEC_STONE_TOMB                        0xD7
 
 #define METLOC_SPECIAL_EGG                  0xFD
 #define METLOC_IN_GAME_TRADE                0xFE
