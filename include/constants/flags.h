fab0c38665b0488ff1611456344f66323c296c0c
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2023-09-13 16:31:30 +0200

    Added more door scripts, along with a Rocket NPC

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -931,13 +931,12 @@
 #define FLAG_HORROR_POWER_RESTORED               0x388
 #define FLAG_HORROR_ROCKET_MOVED               0x389
 #define FLAG_RED_DOOR_SHORTCUT               0x38A
-
+#define FLAG_BLUE_DOOR_POKE               0x38B
+#define FLAG_BLUE_DOOR_QUARTER               0x38C
+#define FLAG_YELLOW_DOOR               0x38D
+#define FLAG_HORROR_ROCKET_EXPLAINED               0x38E
 
 // Unused?
-#define FLAG_0x38B               0x38B
-#define FLAG_0x38C               0x38C
-#define FLAG_0x38D               0x38D
-#define FLAG_0x38E               0x38E
 #define FLAG_0x38F               0x38F
 #define FLAG_0x390               0x390
 #define FLAG_0x391               0x391
