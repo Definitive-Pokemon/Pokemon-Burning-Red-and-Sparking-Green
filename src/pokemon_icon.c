89cabcf493b6877f7a07fa318f58871d3919435a
ElwinBran <elwinbran@gmail.com>
2023-10-10 19:10:12 +0200

    F Kabutops graphics fix attempt

--- src/pokemon_icon.c
+++ src/pokemon_icon.c
@@ -910,7 +910,7 @@ const u8 gMonIconPaletteIndices[] = {
     [SPECIES_UNOWN_Z]     = 0,
     [SPECIES_UNOWN_EMARK] = 0,
     [SPECIES_UNOWN_QMARK] = 0,
-    [SPECIES_FOSSILIZED_KABUTOPS] = 0
+    [SPECIES_FOSSILIZED_KABUTOPS] = 1
 };
 
 const struct SpritePalette gMonIconPaletteTable[] = {
